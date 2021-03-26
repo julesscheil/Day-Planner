7bdc752b2cea1731e72acd1976e5d1fbea748965
Jules Scheil <juliana.m.scheil@gmail.com>
2021-03-26 19:38:49 -0400

    Set up scriptjs and started hardcoding html for testing

---
+++ script.js
@@ -0,0 +1,14 @@
+// DOM Variables
+var currentDay=$("#currentDay");
+
+// Javascript Variables
+currentDayTitle=moment().format("dddd, MMMM Do YYYY");
+currentDay.text(currentDayTitle);
+
+var hour=moment().format("H");
+
+// Function Definitions
+
+// Event Listeners
+
+// Function Calls
