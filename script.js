9e0525a8f1f1ed3dd1e97bdfa884bd149af2171e
Jules Scheil <juliana.m.scheil@gmail.com>
2021-03-27 17:59:29 -0400

    added click save button functionality

--- script.js
+++ script.js
@@ -1,30 +1,41 @@
-// DOM Variables
+// Variables
 var currentDay=$("#currentDay");
-
-// Javascript Variables
+// Get title date
 currentDayTitle=moment().format("dddd, MMMM Do YYYY");
 currentDay.text(currentDayTitle);
 
+// text box saver on click of save button
+$('.saveBtn').click(function() {
+    var eventContents = $('textarea').val();
+    console.log(eventContents);
+    localStorage.setItem("eventContents", eventContents);
+    //send to server and process response
+});
 
-// Function Definitions
 
-function hourColors() {
+
+    // Get current hour
     var currentHour = moment().format("HH");
 
+    // For each id with a class of description, set the following
     $(".description").each(function () {
+        // Get value of ID in class = description
         var hourID = $(this).attr("id");
-        console.log(hourID);
-        console.log(currentHour);
+        // If  hour value is less than current hour set class to past
         if (hourID<currentHour) {
-            console.log("past");
-        } else if (hourID==currentHour) {
-
+            $(this).addClass("past");
+            $(this).removeClass("future","present");
+        } 
+        // If  hour value is less than current hour set class to present
+        else if (hourID===currentHour) {
+            $(this).addClass("present");
+            $(this).removeClass("future","past");
+        } 
+        // If  hour value is less than current hour set class to future
+        else if (hourID>currentHour) {
+            $(this).addClass("future");
+            $(this).removeClass("present","past");
         }
        
     })
-};
 
-// Event Listeners
-
-// Function Calls
-hourColors();
